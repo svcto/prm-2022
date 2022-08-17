ff0e8c7ff6212f2e843eddcf6d160f0aa7ec7857
Samuel Victor Alves Antunes <samuewicto.alves@gmail.com>
2022-08-16 21:44:26 -0300

    Aula 16/08
    Aula 16/08

---
+++ loja-backend/src/controller/CategoryController.ts
@@ -0,0 +1,97 @@
+import { Category } from './../entity/Category';
+import { Request, Response } from "express";
+import { TypeORMError } from 'typeorm';
+
+
+class CategoryController {
+
+    public async index(request: Request, response: Response) {
+        try {
+            // buscar todos os registros
+            const found = await Category.find();
+
+            return response.json(found);
+        } catch (e) {
+            const error = e as TypeORMError;
+            return response.status(500).json({ message: error.message });
+        }
+    }
+
+    public async create(request: Request, response: Response) {
+        try {
+            // cria um registro
+            const obj = await Category.save(request.body);
+
+            return response.status(201).json(obj);
+        } catch (e) {
+            const error = e as TypeORMError;
+            return response.status(500).json({ message: error.message });
+        }
+    }
+
+    public async show(request: Request, response: Response) {
+        try {
+            const {id} = request?.params;
+            if (!id) {
+                return response.status(400).json({message: 'Faltou o id'})
+            }
+            
+            // buscar todos os registros
+            const found = await Category.findOneBy({id: Number(id)});
+            if (!found) {
+                return response.status(404).json({message: 'Registro não encontrador'})
+            }
+
+            return response.json(found);
+        } catch (e) {
+            const error = e as TypeORMError;
+            return response.status(500).json({ message: error.message });
+        }
+    }
+
+    public async update(request: Request, response: Response) {
+        try {
+            const {id} = request?.params;
+            if (!id) {
+                return response.status(400).json({message: 'Faltou o id'})
+            }
+            
+            // buscar todos os registros
+            const found = await Category.findOneBy({id: Number(id)});
+            if (!found) {
+                return response.status(404).json({message: 'Registro não encontrador'})
+            }
+
+            const saved = await Category.update(request.body.id, request.body)
+
+            return response.json(saved);
+        } catch (e) {
+            const error = e as TypeORMError;
+            return response.status(500).json({ message: error.message });
+        }
+    }
+
+    public async remove(request: Request, response: Response) {
+        try {
+            const {id} = request?.params;
+            if (!id) {
+                return response.status(400).json({message: 'Faltou o id'})
+            }
+            
+            // buscar todos os registros
+            const found = await Category.findOneBy({id: Number(id)});
+            if (!found) {
+                return response.status(404).json({message: 'Registro não encontrador'})
+            }
+
+            await found.remove();
+
+            return response.status(204).json();
+        } catch (e) {
+            const error = e as TypeORMError;
+            return response.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export default new CategoryController();
